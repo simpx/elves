549a4565144d818cfc187b4d51f1e893d76fdf01
simpx <simpxx@mail.com>
2012-09-16 23:01:42 +0800

    v0.0.2

--- build/lib/elves/elves.py
+++ build/lib/elves/elves.py
@@ -1,12 +1,11 @@
 '''
-Eleves -- Python Threading For Human
+elves -- Python Threading For Human
 '''
 import threading
 import Queue
 from functools import partial
 
-queue = Queue.Queue()
-threads = []
+last_elves = None
 
 class Elf(threading.Thread):
 
@@ -20,14 +19,37 @@ class Elf(threading.Thread):
             job()
             self.queue.task_done()
 
-def run(num):
-    for i in range(num):
-        threads.append(Elf(queue))
-        threads[-1].setDaemon(True)
-        threads[-1].start()
+class Elves():
+    def __init__(self, num):
+        self.num = num
+        self.queue = Queue.Queue()
+        self.threads = []
+
+    def run(self):
+        for i in range(self.num):
+            self.threads.append(Elf(self.queue))
+            self.threads[-1].setDaemon(True)
+            self.threads[-1].start()
+
+    def put(self, func, *args, **kwargs):
+        self.queue.put(partial(func, *args, **kwargs))
+
+    def join(self):
+        self.queue.join()
+
+
+def run(num, func=None, *args, **kwargs):
+    global last_elves
+    elves = Elves(num)
+    elves.run()
+    last_elves = elves
+    if func is not None:
+        for i in range(num):
+            elves.put(func, *args, **kwargs)
+    return last_elves
 
 def put(func, *args, **kwargs):
-    queue.put(partial(func, *args, **kwargs))
+    last_elves.put(func, *args, **kwargs)
 
 def join():
-    queue.join()
+    last_elves.join()
